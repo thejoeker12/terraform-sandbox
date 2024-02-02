6cad626f4477a074dd1ed8cefe32291281be92cb
Joseph Little <joelittle11@live.com>
2024-02-02 09:33:48 +0000

    Refactor infra.tf: Removed commented out code and added new resources

--- infra.tf
+++ infra.tf
@@ -1,22 +1,10 @@
-# resource "jamfpro_scripts" "scripts_0003_JL" {
-#   name            = "[scpt]-JL-TEST"
-#   script_contents = "echo 'Hello World!'"
-#   os_requirements = "13"
-#   priority        = "BEFORE"
-#   info            = "Adds target user or group to specified group membership, or removes said membership."
-#   notes           = "Jamf Pro script parameters 4 -> 7"
-#   parameter4  = "100" // targetID
-#   parameter5  = "group" // Target Type - Must be either "user" or "group"
-#   parameter6  = "someGroupName" // targetMembership
-#   parameter7  = "add" // Script Action - Must be either "add" or "remove"
-#   parameter10 = "this is a param"
-# } 
 
 
-resource "jamfpro_departments" "the_name" {
-name = "Test Department" 
+
+resource "jamfpro_departments" "Test_Dept" {
+    name = "Test Dept"
 }
 
-# resource "jamfpro_departments" "the_other_name" {
-# name = "Test Department2" 
-# }
+resource "jamfpro_byoprofiles" "test" {
+    name = "Test Profile" 
+}
