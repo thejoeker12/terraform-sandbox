4e22e3592dc3f1bd6788da1679fc92a43060b4b1
Joseph Little <joelittle11@live.com>
2024-02-13 15:02:11 +0000

    renamed:    h_apply_debug.sh -> apply_debug.sh 	renamed:    h_compile.sh -> compile.sh 	renamed:    h_compile_apply_debug.sh -> compile_apply_debug.sh 	renamed:    h_del_state.sh -> del_state.sh 	modified:   h_custom.sh 	modified:   infra.tf

--- h_custom.sh
+++ h_custom.sh
@@ -1,3 +1,3 @@
 clear
-sh h_compile.sh
+sh compile.sh
 TF_LOG=DEBUG terraform apply --auto-approve
--- infra.tf
+++ infra.tf
@@ -1,13 +1,13 @@
 resource "jamfpro_macos_configuration_profile" "Scope_Test" {
     name = "Scope Test config profile"
-    category  {
-        id = 5
-        name = "Applications"
-    }
     scope {
         computers {
-            id = [16, 18, 14, 15, 17]
+            id = [
+                17,
+                18,
+                14,
+                16
+            ]
         }
     }
-
 }
