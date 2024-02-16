08d92a4cb9e861166bdd390a6de40bed2749397d
Joseph Little <joelittle11@live.com>
2024-02-16 01:25:45 +0000

    Refactor infra.tf: Uncomment and update category configuration

--- infra.tf
+++ infra.tf
@@ -1,13 +1,21 @@
 resource "jamfpro_macos_configuration_profile" "Scope_Test" {
     name = "Scope Test config profile"
-    scope {
-        computers {
-            id = [
-                17,
-                18,
-                14,
-                16
-            ]
-        }
+    # site {
+    #     id = 661
+    #     name = "jl_test"
+    # }
+    category {
+       id = 5
+       name = "Applications"
     }
+    # scope {
+    #     computers {
+    #         id = [
+    #             17,
+    #             18,
+    #             14,
+    #             16
+    #         ]
+    #     }
+    # }
 }
