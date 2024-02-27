9471662df756ccbb1c79714e42c962792ab4141d
Joseph Little <joelittle11@live.com>
2024-02-27 17:36:17 +0000

    Refactor scope in infra.tf

--- infra.tf
+++ infra.tf
@@ -1,21 +1,8 @@
-
 resource "jamfpro_macos_configuration_profile" "Scope_Test" {
     name = "Scope Test config profile"
-    category {
-       id = 5
-       name = "Applications"
-    }
-    description = "This is the description of the configuration profile"
-    user_removeable = false
-    level = "User"
     scope {
         computers {
-          id = sort([
-            15,
-            18,
-            17
-          ])
+            id = sort([15, 14])
         }
     }
-
 }
