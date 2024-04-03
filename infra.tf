bf2a9b4985953e9a78143fe6774145af117ef1da
Joseph Little <joelittle11@live.com>
2024-04-03 16:30:57 +0100

    bugfixing

--- infra.tf
+++ infra.tf
@@ -1,20 +1,10 @@
-
-resource "jamfpro_macos_configuration_profile" "Scope_Test2" {
-  name                = "Resource Testing-test"
-  distribution_method = "Install Automatically"
-  payload = file("payload.mobileconfig")
-  category {
-    id = -1
-  }
-  scope {
-    all_computers      = false
-    computer_ids       = sort([17, 18])
-    computer_group_ids = sort([53])
-    jss_user_ids       = [4]
-    jss_user_group_ids = [4]
-
-    exclusions {
-      department_ids     = [27653]
+resource "jamfpro_policy" "policy_testing" {
+    name = "test name"
+    scope {}
+    override_default_settings {}
+    self_service {}
+    site {
+      
     }
-  }
+    enabled = false
 }
