37d8fb7145d15eaf770d42015b9c16fbe6ff7915
Joseph Little <joelittle11@live.com>
2024-04-16 16:25:40 +0100

    policy layout testing

--- infra.tf
+++ infra.tf
@@ -1,10 +1,46 @@
+
 resource "jamfpro_policy" "policy_testing" {
     name = "test name"
-    scope {}
+    enabled = false
+    scope {
+        all_computers = false
+        all_jss_users = false
+    }
     override_default_settings {}
     self_service {}
+
     site {
-      
+        id = 0
+        name = "none"
     }
-    enabled = false
+
+    payloads {
+
+        // Packages
+        packages {
+            package {
+                id = 1
+                fill_user_template = ""
+            }
+
+            package {
+                id = 2
+            }
+
+            package {
+                id = 3
+            }
+        }
+        
+        // Scripts
+        scripts {
+            script {
+                id = 1
+            }
+
+            script {
+                id = 4
+            }
+        }
+    }
 }
