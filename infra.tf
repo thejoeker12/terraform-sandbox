c302f4c7c464ae9f64b7d4443fdb8e0b1e3d3da6
Joseph Little <joelittle11@live.com>
2024-04-25 16:03:01 +0100

    debugging

--- infra.tf
+++ infra.tf
@@ -1,8 +1,9 @@
 
 resource "jamfpro_policy" "policy_testing" {
     name = "Test - Managed by TF1"
-    enabled = true
+    enabled = false
     network_limitations {
       minimum_network_connection = "Ethernet"
     }
+    
 }
