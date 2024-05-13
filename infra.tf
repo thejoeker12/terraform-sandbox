a0442a57db58d209e797a44d354023e0425b374c
Joseph Little <joelittle11@live.com>
2024-05-13 14:15:35 +0100

    debug

--- infra.tf
+++ infra.tf
@@ -14,6 +14,15 @@ resource "jamfpro_policy" "policy_testing" {
       # computer_group_ids = sort([78])
       # department_ids = [37287]
     }
+
+    self_service {
+      use_for_self_service = true
+      self_service_display_name = "New Self Service Name"
+      install_button_text = "YES"
+      self_service_description = "New self service description"
+      force_users_to_view_description = true
+      feature_on_main_page = true
+    }
 }
 
 # resource "jamfpro_macos_configuration_profile" "tes2t" {
