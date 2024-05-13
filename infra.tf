8dd473a7c0445e38e9c8524b9cc8389443a12bc2
Joseph Little <joelittle11@live.com>
2024-05-13 16:06:09 +0100

    debug

--- infra.tf
+++ infra.tf
@@ -3,16 +3,18 @@ resource "jamfpro_policy" "policy_testing" {
     name = file("policy_name.txt")
     enabled = false
     trigger_checkin = true
-    frequency = "Once every day"
+    # frequency = "Once every day"
     category {
       id = 5
     }
     scope {
       all_computers = false
       all_jss_users = true
-      # computer_ids = sort([15, 16])
-      # computer_group_ids = sort([78])
-      # department_ids = [37287]
+      computer_ids = sort([15, 16])
+      department_ids = [37287]
+      exclusions {
+        computer_group_ids = sort([78])
+      }
     }
 
     self_service {
@@ -23,13 +25,18 @@ resource "jamfpro_policy" "policy_testing" {
       force_users_to_view_description = true
       feature_on_main_page = true
     }
+
+    payloads {
+      packages {
+        package {
+          id = 232
+        }
+        package {
+          id = 233
+        }
+      }
+    }
+
+  
 }
 
-# resource "jamfpro_macos_configuration_profile" "tes2t" {
-#   name = "A completely new name"
-#   scope {
-#     all_computers = false
-#   }
-#   payload = file("/Users/joseph/Desktop/test.mobileconfig")
-
-# }
