9e49978765a0f81981c528cdd6098f192e5c170e
Joseph Little <joelittle11@live.com>
2024-05-24 15:06:34 +0100

    debug

--- modupdate.sh
+++ modupdate.sh
@@ -1,4 +1,5 @@
 echo "updating provider modules..."
+
 cd ../terraform-provider-jamfpro
 go clean --cache
 GOPROXY=direct go get -u github.com/deploymenttheory/go-api-sdk-jamfpro@jl-dev
@@ -8,6 +9,7 @@ echo "provider module update complete"
 
 echo "updating sdk modules..."
 cd ../go-api-sdk-jamfpro
+go clean --cache
 GOPROXY=direct go get -u github.com/deploymenttheory/go-api-http-client@latest
 go mod tidy
 echo "sdk modules update complete"
