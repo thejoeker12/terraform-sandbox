d17ea5db79987481162b2cfb5c26fe642cca2d6c
Joseph Little <joelittle11@live.com>
2024-05-24 09:59:30 +0100

    debug

--- update_provider_httpclient.sh
+++ update_provider_httpclient.sh
@@ -2,7 +2,7 @@ cd ../terraform-provider-jamfpro
 GOPROXY=direct go get -u github.com/deploymenttheory/go-api-http-client@latest
 go mod tidy
 cd ../go-api-sdk-jamfpro
-GOPROXY=direct go get -u github.com/deploymenttheory/go-api-http-client@fix-cookiejar
+GOPROXY=direct go get -u github.com/deploymenttheory/go-api-http-client@latest
 go mod tidy
 cd ../terraform-sandbox
 sh compile.sh
