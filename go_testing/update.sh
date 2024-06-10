ff05fc7e77478e6c7715ddb205d7a1a09e590f3b
Joseph Little <joelittle11@live.com>
2024-06-10 11:16:59 +0100

    updated helper

--- go_testing/update.sh
+++ go_testing/update.sh
@@ -1,6 +1,6 @@
 clear
 echo "updating sdk..."
-GOPROXY=direct go get -u "github.com/deploymenttheory/go-api-sdk-jamfpro@jl-dev"
+GOPROXY=direct go get -u "github.com/deploymenttheory/go-api-sdk-jamfpro@dev-jl-httpclientv2"
 echo "updating integration..."
 GOPROXY=direct go get -u "github.com/deploymenttheory/go-api-http-client-integration-jamfpro/jamfprointegration"
 echo "updating http client..."
