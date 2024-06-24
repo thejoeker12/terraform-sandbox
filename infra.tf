15e0193601ff6a6a5b6f20048b6416a374909ded
Joseph Little <joelittle11@live.com>
2024-06-24 17:21:52 +0100

    debugging

--- infra.tf
+++ infra.tf
@@ -10,14 +10,45 @@
 
 
 
-resource "jamfpro_script" "scripts_0001" {
-  name            = "something else-1"
-  script_contents = "hello world"
-  # category_id = 5
-  priority        = "BEFORE"
-  info = "some script info"
-}
+# resource "jamfpro_script" "scripts_0001" {
+#   count = 25
+#   name            = "Script Volume Test ${count.index}"
+#   script_contents = "hello world"
+#   category_id = 5
+#   priority        = "BEFORE"
+#   info = "some script info"
+# }
 
+
+resource "jamfpro_smart_computer_group" "NEw" {
+  count = 25
+  name = "JL Computer Group NEW ${count.index}"
+  
+  # Optional: Specify site details 
+  site_id = 1174
+
+  # Optional: Define criteria for Smart groups
+  # criteria {
+  #   name          = "Criterion Name #1"
+  #   priority      = 0      # 0 is the highest priority, 1 is the next highest, etc.
+  #   and_or        = "and"  # or "or", defaults to "and" if not provided
+  #   search_type   = "is"   # or any other supported search type
+  #   value         = "Criterion Value"
+  #   opening_paren = false  # true or false, defaults to false if not provided
+  #   closing_paren = false  # true or false, defaults to false if not provided
+  # }
+
+  # criteria {
+  #   name          = "Criterion Name #n+1 etc"
+  #   priority      = 1
+  #   and_or        = "and"  # or "or", defaults to "and" if not provided
+  #   search_type   = "is"   # or any other supported search type
+  #   value         = "Criterion Value"
+  #   opening_paren = false  # true or false, defaults to false if not provided
+  #   closing_paren = false  # true or false, defaults to false if not provided
+  # }
+
+}
 
 
 
