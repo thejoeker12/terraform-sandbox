9c993f0ced0ac1b2a692724f21777b867a7ba5c3
Joseph Little <joelittle11@live.com>
2024-06-24 09:19:30 +0100

    debugging

--- infra.tf
+++ infra.tf
@@ -1,8 +1,16 @@
-resource "jamfpro_static_computer_group" "jamfpro_static_computer_group_001" {
-  name = "Example Static Computer Group"
+# resource "jamfpro_static_computer_group" "jamfpro_static_computer_group_001" {
+#   name = "Site testing"
 
-  site {
-    id = 1173  
-  }
+#   # site_id = 1173
+#   site_id = 1174
+
+# }
+
+resource "jamfpro_script" "scripts_0002" {
+  name            = "Category Testing Script"
+  script_contents = "hello world"
+  os_requirements = "13.1"
+  priority        = "BEFORE"
+  info = "Your script info here."
 
 }
