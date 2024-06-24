b6667d1e930efdef0c0011cf7b6816de415b5e30
Joseph Little <joelittle11@live.com>
2024-06-24 16:20:09 +0100

    debugging

--- infra.tf
+++ infra.tf
@@ -6,11 +6,54 @@
 
 # }
 
-resource "jamfpro_script" "scripts_0002" {
-  name            = "Category Testing Script"
-  script_contents = "hello world"
-  os_requirements = "13.1"
-  priority        = "BEFORE"
-  info = "Your script info here."
 
+
+
+
+resource "jamfpro_script" "scripts_0001" {
+  name            = "something else-1"
+  script_contents = "hello world"
+  # category_id = 5
+  priority        = "BEFORE"
+  info = "some script info"
 }
+
+
+
+
+
+
+
+
+
+
+
+# resource "jamfpro_script" "scripts_0002" {
+#   name            = "Category Testing Script-cleanuptest-2"
+#   script_contents = "hello world"
+#   os_requirements = "13.1"
+#   priority        = "BEFORE"
+#   info = "Your script info here."
+#   timeouts {
+#     create = "50m"
+#   }
+
+# }
+
+# resource "jamfpro_script" "scripts_0003" {
+#   name            = "Category Testing Script-cleanuptest-3"
+#   script_contents = "hello world"
+#   os_requirements = "13.1"
+#   priority        = "BEFORE"
+#   info = "Your script info here."
+
+# }
+
+# resource "jamfpro_script" "scripts_0004" {
+#   name            = "Category Testing Script-cleanuptest-4"
+#   script_contents = "hello world"
+#   os_requirements = "13.1"
+#   priority        = "BEFORE"
+#   info = "Your script info here."
+
+# }
