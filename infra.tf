c99df75ebd81aaa2aede7a99b989a2691a3d2d0f
thejoeker12 <joelittle11@live.com>
2024-06-25 13:38:48 +0100

    debugging

--- infra.tf
+++ infra.tf
@@ -6,15 +6,15 @@
 # }
 
 
-resource "jamfpro_advanced_computer_search" "test_search" {
-  count = 10
-  name = "Test name - ${count.index}"
-  # site_id = 1174
-}
+# resource "jamfpro_advanced_computer_search" "test_search" {
+#   count = 10
+#   name = "Test name - ${count.index}"
+#   # site_id = 1174
+# }
 
 resource "jamfpro_script" "scripts_0002" {
   count = 25
-  name            = "Category Testing Script-cleanuptest-${count.index}"
+  name            = "Category Testing Script-home-${count.index}"
   script_contents = "hello world"
   os_requirements = "13.1"
   priority        = "BEFORE"
