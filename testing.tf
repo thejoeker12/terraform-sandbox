1b399d22c7b2ac56ce9ea54d86ae90cf62a4631b
Joseph Little <joelittle11@live.com>
2024-06-27 13:19:27 +0100

    debugging

---
+++ testing.tf
@@ -0,0 +1,34 @@
+
+
+variable "global_name_prefix" {
+    type = string
+    default = "Jl-Testing"
+  
+}
+
+
+
+resource "jamfpro_api_integration" "jamfpro_api_integration_001" {
+  display_name                    = "${var.global_name_prefix}-apiintegration-1"
+  enabled                         = true
+  access_token_lifetime_seconds   = 7200
+  authorization_scopes            = [jamfpro_api_role.jamfpro_api_role_002.display_name]
+}
+
+resource "jamfpro_api_role" "jamfpro_api_role_002" {
+  display_name = "${var.global_name_prefix}-apirole-1"
+  privileges   = ["Create API Roles", "Update API Roles", "Read API Roles", "Delete API Roles"]
+}
+
+
+data "jamfpro_api_integration" "Testing" {
+    id = jamfpro_api_integration.jamfpro_api_integration_001.id
+}
+
+output "test2" {
+  value = data.jamfpro_api_integration.Testing.client_id
+}
+
+output "test3" {
+  value = data.jamfpro_api_integration.Testing.client_secret
+}
