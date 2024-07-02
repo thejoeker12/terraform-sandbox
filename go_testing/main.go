71a15f9bd641caa47af808c9cfa6c7a36793d0dc
Joseph Little <joelittle11@live.com>
2024-07-02 09:45:32 +0100

    debugging

--- go_testing/main.go
+++ go_testing/main.go
@@ -1,14 +1,49 @@
 package main
 
 import (
+	"time"
+
 	"go.uber.org/zap"
 )
 
 func main() {
-	logger, _ := zap.NewProduction()
+	config := zap.NewProductionConfig()
 
-	logger.Info("INFO!")
+	config.OutputPaths = []string{"stdout", "testlogfile.txt"}
+
+	logger, _ := config.Build()
+	sugar := logger.Sugar()
 
 	defer logger.Sync()
 
+	sugar.Infow("failed to fetch URL",
+		"url", "http://example.com",
+		"attempt", 3,
+		"backoff", time.Second,
+	)
+
+	sugar.Infow("failed to fetch URL",
+		"url", "http://example.com",
+		"attempt", 3,
+		"backoff", time.Second,
+		zap.String("thing", "value"),
+	)
+
+	sugar.Infow("failed to fetch URL",
+		"url", "http://example.com",
+		"attempt", 3,
+		"backoff", time.Second,
+	)
+
+	sugar.Infow("failed to fetch URL",
+		"url", "http://example.com",
+		"attempt", 3,
+		"backoff", time.Second,
+	)
+
+	sugar.Infow("failed to fetch URL",
+		"url", "http://example.com",
+		"attempt", 3,
+		"backoff", time.Second,
+	)
 }
