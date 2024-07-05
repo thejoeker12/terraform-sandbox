3e4a89a59c11650e4a1547936d184d0a68338e72
Joseph Little <joelittle11@live.com>
2024-07-05 14:09:42 +0100

    debugging

--- infra.tf
+++ infra.tf
@@ -1,57 +1,5 @@
-# resource "jamfpro_static_computer_group" "jamfpro_static_computer_group_newlogger" {
-#   name = "Flattened Test_newlogger"
-#   assigned_computer_ids = [16]
-
-# }
-
-# resource "jamfpro_category" "example_category_newlogger" {
-#   name = "Anoteher name_newlogger"
-#   priority = 1
-# }
-
-# resource "jamfpro_script" "scripts_newlogger" {
-#   name            = "tf-example-script-inline_newlogger-home"
-#   script_contents = "hello world"
-#   os_requirements = "13.1"
-#   priority        = "BEFORE"
-#   info = "Your script info here."
-#   notes = ""
-#   category_id = jamfpro_category.example_category_newlogger.id
-
-# }
-
-# resource "jamfpro_account_group"  "jamf_pro_account_group_002" {
-#   name         = "jl testing"
-#   access_level = "Full Access" // Full Access / Site Access / Group Access
-#   privilege_set = "Custom" 
-
-#   jss_objects_privileges = [
-#     "Create Advanced Computer Searches",
-#     "Read Advanced Computer Searches",
-#     "Update Advanced Computer Searches",
-#     "Delete Advanced Computer Searches",
-#     "Create Advanced Mobile Device Searches",
-#     "Read Advanced Mobile Device Searches",
-#     "Update Advanced Mobile Device Searches",
-#     "Delete Advanced Mobile Device Searches",
-#     "Create Advanced User Searches",
-#     "Read Advanced User Searches",
-#     "Update Advanced User Searches",
-#     "Delete Advanced User Searches",
-#     "Create Advanced User Content Searches",
-#     "Read Advanced User Content Searches",
-#     "Update Advanced User Content Searches",
-#     "Delete Advanced User Content Searches",
-#     "Create AirPlay Permissions",
-#     "Read AirPlay Permissions",
-#     "Update AirPlay Permissions",
-#     "Delete AirPlay Permissions",
-#   ]
-
-# }
-
 resource "jamfpro_policy" "jamfpro_policy_script_example" {
-  name                          = "tf-localtest-policy-script-example"
+  name                          = "JL policy testing"
   enabled                       = false
   trigger_checkin               = false
   trigger_enrollment_complete   = false
@@ -89,33 +37,28 @@ resource "jamfpro_policy" "jamfpro_policy_script_example" {
   }
 
   payloads {
+    # packages {
+    #   distribution_point = "default" // Set the appropriate distribution point
+    #   package {
+    #     id                          = 367
+    #     action                      = "Cache" // The action to perform with the package (e.g., Install, Cache, etc.)
+    #     fill_user_template          = false     // Whether to fill the user template
+    #     fill_existing_user_template = false     // Whether to fill existing user templates
+    #   }
+    # }
 
     scripts {
       id          = 7722
       priority    = "After"
-      parameter4  = "param_value_4"
-      parameter5  = "param_value_5"
-      parameter6  = "param_value_6"
-      parameter7  = "param_value_7"
-      parameter8  = "param_value_8"
-      parameter9  = "param_value_9"
-      parameter10 = "param_value_10"
-      parameter11 = "param_value_11"
-
     }
+
     scripts {
       id          = 7719
       priority    = "After"
-      parameter4  = "param_value_4"
-      parameter5  = "param_value_5"
-      parameter6  = "param_value_6"
-      parameter7  = "param_value_7"
-      parameter8  = "param_value_8"
-      parameter9  = "param_value_9"
-      parameter10 = "param_value_10"
-      parameter11 = "param_value_11"
     }
 
+    reboot {
+    }
 
   }
 
