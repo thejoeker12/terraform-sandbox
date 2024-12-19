977181d7733abb58e62afa83b84f9c315c808eda
Joseph <joelittle11@live.com>
2024-12-19 15:04:39 +0000

    interface eg

--- go_testing/go.mod
+++ go_testing/go.mod
@@ -3,3 +3,7 @@ module test
 go 1.22.2
 
 toolchain go1.22.3
+
+require golang.org/x/crypto v0.30.0
+
+require golang.org/x/sys v0.28.0 // indirect
--- go_testing/main.go
+++ go_testing/main.go
@@ -2,14 +2,74 @@ package main
 
 import (
 	"fmt"
-	"net/url"
 )
 
+type Engine struct {
+	Displacement float64
+	CylCount     int
+}
+
+type Vehicle interface {
+	Start()
+	GetStatus() bool
+}
+
 func main() {
-	test_data := "https://joe.com/i%20am%20a%20test"
-	parsed, _ := url.Parse(test_data)
-	test := parsed.Path
+	BMW := &Car{
+		Make:  "BMW",
+		Model: "3 Series",
+		Age:   10,
+		Engine: Engine{
+			Displacement: 3.999,
+			CylCount:     6,
+		},
+	}
 
-	fmt.Println(test)
+	Iveco := &Lorry{
+		Name: "jeff",
+	}
 
+	fmt.Println(BMW.Started)
+	fmt.Println(StartVehicle(BMW))
+
+	fmt.Println(Iveco.Started)
+	fmt.Println(StartVehicle(Iveco))
+
+}
+
+func StartVehicle(v Vehicle) bool {
+	v.Start()
+	return v.GetStatus()
+
+}
+
+// Car
+type Car struct {
+	Started bool
+	Make    string
+	Model   string
+	Age     int
+	Engine  Engine
+}
+
+func (c *Car) Start() {
+	c.Started = true
+}
+
+func (c *Car) GetStatus() bool {
+	return c.Started
+}
+
+// Lorry
+type Lorry struct {
+	Started bool
+	Name    string
+}
+
+func (c *Lorry) Start() {
+	c.Started = true
+}
+
+func (c *Lorry) GetStatus() bool {
+	return c.Started
 }
