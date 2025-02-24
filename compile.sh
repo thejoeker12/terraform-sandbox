4f4870523f16be6207c8ec88df410d1327cab359
bobby williams <bobby2000williams@gmail.com>
2025-02-24 12:20:09 +0000

    Uses environment variables now

--- compile.sh
+++ compile.sh
@@ -1,12 +1,18 @@
+#!/bin/sh
+
+# LOCAL_PROVIDER_ENVIRONMENT will be the architecture of your machine, eg linux_amd64, darwin_arm64 etc
+echo "sourcing .env file..."
+. ./.env
+
 echo "removing .terraform..."
 rm -rf .terraform
 
 echo "removing .terraform.lock.hcl..."
 rm -rf .terraform.lock.hcl 
 
+echo "local provider environment: $LOCAL_PROVIDER_ENVIRONMENT"
 echo "removing compiled provider plugin"
-rm -rf ~/.terraform.d/plugins/terraform.local/local/jamfpro/0.1.0/darwin_arm64/terraform-provider-jamfpro
-
+rm -rf ~/.terraform.d/plugins/terraform.local/local/jamfpro/0.1.0/$LOCAL_PROVIDER_ENVIRONMENT/terraform-provider-jamfpro
 cd ..
 cd terraform-provider-jamfpro
 
@@ -14,11 +20,11 @@ echo "rebuilding binary"
 go build
 
 echo "moving binary to plugins folder & adjusting permissions"
-mkdir -p ~/.terraform.d/plugins/terraform.local/local/jamfpro/0.1.0/darwin_arm64/
-mv terraform-provider-jamfpro ~/.terraform.d/plugins/terraform.local/local/jamfpro/0.1.0/darwin_arm64/
-chmod +x ~/.terraform.d/plugins/terraform.local/local/jamfpro/0.1.0/darwin_arm64/terraform-provider-jamfpro
+mkdir -p ~/.terraform.d/plugins/terraform.local/local/jamfpro/0.1.0/$LOCAL_PROVIDER_ENVIRONMENT/
+mv terraform-provider-jamfpro ~/.terraform.d/plugins/terraform.local/local/jamfpro/0.1.0/$LOCAL_PROVIDER_ENVIRONMENT/
+chmod +x ~/.terraform.d/plugins/terraform.local/local/jamfpro/0.1.0/$LOCAL_PROVIDER_ENVIRONMENT/terraform-provider-jamfpro
 
 echo "initiating terraform"
 cd ..
-cd terraform-sandbox
+cd terraform-provider-jamfpro-testing
 terraform init
