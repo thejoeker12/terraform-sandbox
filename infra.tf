d1a98d55b9cfd3d05eed4720bd06a604957c7a11
Joseph <joelittle11@live.com>
2025-04-01 09:56:58 +0100

    fuck you

--- infra.tf
+++ infra.tf
@@ -1,11 +1,21 @@
-# resource "jamfpro_script" "script1" {
-#   count = 2
-#   name = "tf-testing-local-jl-${count.index}"
-#   script_contents = "echo hello world"
-#   priority = "BEFORE"
-#   timeouts {
-#     create = "100s"
-#     delete = "100s"
-#   }
-# }
+resource "jamfpro_script" "script1" {
+  count           = 2
+  name            = "tf-testing-local-jl-5-${count.index}"
+  script_contents = "echo hello world"
+  priority        = "BEFORE"
+}
 
+
+resource "jamfpro_script" "script2" {
+  count           = 5
+  name            = "tf-testing-local-jl-6-${count.index}"
+  script_contents = "echo hello world"
+  priority        = "BEFORE"
+}
+
+resource "jamfpro_building" "build" {
+  name = "cheese"
+#   timeouts {
+#     create = "10m"
+#   }
+}
--- packages.tf
+++ packages.tf
@@ -1,66 +1,15 @@
-resource "jamfpro_package" "jamfpro_package_001" {
-  package_name          = "googlechrome-jl"
-  package_file_source   = "/Users/josephlittle/Github/packages/googlechrome.dmg"
-  category_id           = "-1" // jamfpro_category.jamfpro_category_001.id
-  info                  = "tf package deployment for demonstration"
-  notes                 = "Uploaded by: terraform-provider-jamfpro plugin."
-  priority              = 10
-  reboot_required       = true
-  fill_user_template    = false
-  fill_existing_users   = false
-  os_requirements       = "macOS 10.15.7, macOS 11.1"
-  swu                   = true
-  self_heal_notify      = false
-  os_install            = false
-  serial_number         = ""
-  suppress_updates      = false
-  ignore_conflicts      = false
-  suppress_from_dock    = false
-  suppress_eula         = false
-  suppress_registration = false
-  manifest              = ""
-  manifest_file_name    = ""
-}
-
-resource "jamfpro_package" "jamfpro_package_002" {
-  package_name          = "sus_package.dmg"
-  package_file_source   = "/Users/josephlittle/Github/packages/sus_package.dmg"
-  category_id           = "-1" // jamfpro_category.jamfpro_category_001.id
-  info                  = "tf package deployment for demonstration"
-  notes                 = "Uploaded by: terraform-provider-jamfpro plugin."
-  priority              = 10
-  reboot_required       = true
-  fill_user_template    = false
-  fill_existing_users   = false
-  os_requirements       = "macOS 10.15.7, macOS 11.1"
-  swu                   = true
-  self_heal_notify      = false
-  os_install            = false
-  serial_number         = ""
-  suppress_updates      = false
-  ignore_conflicts      = false
-  suppress_from_dock    = false
-  suppress_eula         = false
-  suppress_registration = false
-  manifest              = ""
-  manifest_file_name    = ""
-}
-
-#  https://go.microsoft.com/fwlink/?linkid=853070 - company portal
-#  https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US - firefox
-
-# resource "jamfpro_package" "jamfpro_package_03" {
-#   package_name          = "tf-localtest-package-httpsourceprovider-test"
-#   package_file_source   = "https://download.mozilla.org/?product=firefox-latest&os=osx&lang=en-US"
-#   category_id           = "-1"
+# resource "jamfpro_package" "jamfpro_package_001" {
+#   package_name          = "googlechrome-jl"
+#   package_file_source   = "/Users/josephlittle/Github/packages/googlechrome.dmg"
+#   category_id           = "-1" // jamfpro_category.jamfpro_category_001.id
 #   info                  = "tf package deployment for demonstration"
 #   notes                 = "Uploaded by: terraform-provider-jamfpro plugin."
 #   priority              = 10
 #   reboot_required       = true
 #   fill_user_template    = false
 #   fill_existing_users   = false
-#   os_requirements       = "macOS 10.15.0"
-#   swu                   = false
+#   os_requirements       = "macOS 10.15.7, macOS 11.1"
+#   swu                   = true
 #   self_heal_notify      = false
 #   os_install            = false
 #   serial_number         = ""
@@ -73,18 +22,18 @@ resource "jamfpro_package" "jamfpro_package_002" {
 #   manifest_file_name    = ""
 # }
 
-# resource "jamfpro_package" "jamfpro_package_04" {
-#   package_name          = "tf-localtest-package-httpsourceprovider-companyportal"
-#   package_file_source   = "https://go.microsoft.com/fwlink/?linkid=853070"
-#   category_id           = "-1"
-#   info                  = "tf package deployment for demonstration - meta"
+# resource "jamfpro_package" "jamfpro_package_002" {
+#   package_name          = "sus_package.dmg"
+#   package_file_source   = "/Users/josephlittle/Github/packages/sus_package.dmg"
+#   category_id           = "-1" // jamfpro_category.jamfpro_category_001.id
+#   info                  = "tf package deployment for demonstration"
 #   notes                 = "Uploaded by: terraform-provider-jamfpro plugin."
 #   priority              = 10
 #   reboot_required       = true
 #   fill_user_template    = false
 #   fill_existing_users   = false
-#   os_requirements       = "macOS 10.15.0"
-#   swu                   = false
+#   os_requirements       = "macOS 10.15.7, macOS 11.1"
+#   swu                   = true
 #   self_heal_notify      = false
 #   os_install            = false
 #   serial_number         = ""
