21830af7ac868a312f95597371a15a2c0ffec9f3
Joseph Little <joelittle11@live.com>
2026-05-27 10:53:24 +0100

    stuff

--- infra.tf
+++ infra.tf
@@ -1,26 +1,12 @@
-resource "jamfpro_app_installer" "jamfpro_app_installer_001" {
-  name            = "010 Editor"
-  app_title_name  = "010 Editor"
-  enabled         = true
-  deployment_type = "SELF_SERVICE"
-  update_behavior = "AUTOMATIC"
-  category_id     = "-1"
-  site_id         = "-1"
-  smart_group_id  = "1"
+resource "jamfpro_smart_computer_group_v2" "issue_1079_has_operator_ldap_ea" {
+  name        = "tf-issue-1079-v2-has-operator-ldap-ea"
+  description = "Repro for provider issue 1079: v2 smart groups should accept has for LDAP extension attributes."
 
-  install_predefined_config_profiles = false
-  trigger_admin_notifications        = false
-
-  notification_settings {
-    notification_message  = "A new update is available"
-    # notification_interval = 0
-    # deadline_message      = "Update deadline approaching"
-    # deadline              = 0
-    # quit_delay            = 0
-    # complete_message      = "Update completed successfully"
-    # relaunch              = true
-    # suppress              = false
+  criteria {
+    and_or      = "and"
+    name        = "IDP - Transitive Group Membership"
+    priority    = 0
+    search_type = "has"
+    value       = "BSG-SomeGroupName"
   }
-
-
 }
